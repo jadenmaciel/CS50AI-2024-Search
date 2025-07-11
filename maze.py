7c741e1402b761108778c02a958ae1b78ad27cad
jadenmaciel <josemaciel@suumail.net>
2025-07-11 16:14:05 -0700

    Added Classes Node & StackFrontier. Added methods within these classes.

--- maze.py
+++ maze.py
@@ -1,15 +1,24 @@
+# Importing the sys module, which provides access to system-specific parameters and functions
 import sys
 
 # Implementation of maze solving
 
-# Class Node which keeps track of the state, the parent, and the action
+# Blueprint called Node.
+# Special instruction that runs automatically whenerver you create a new 'Node'.
+# Self refers to the specific Node you are creating.
+# The state stores it inside the Node. If you given a state of "Position A," it saves "Position A" as this Node's state.
+# The parent is likely the Node that came right before this one. It helps you trace your steps backward.
+# This stores the action that was taken to get from the parent Node to this new Node. For example, the action might be "move left."
 class Node():
     def __init__(self, state, parent, action):
         self.state = state
         self.parent = parent
         self.action = action
 
-# Class Stack Frontier
+# Blueprint called Stack Frontier.
+# This is the special setup instruction. It automatically runs the moment you create a new StackFrontier.
+# "Give this new backpack (self) an empty list ([]) and name that list frontier."
+# Every time you create a StackFrontier, it starts out with its own empty list inside, ready for you to add things to it.
 class StackFrontier():
     def __init__(self):
         self.frontier = []
